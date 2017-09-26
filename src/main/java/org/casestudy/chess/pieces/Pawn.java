ecfd459186f2c0f6c594ae9085cb5294f416a912
Aditya Bhasin <adbhasin@cisco.com>
2017-09-26 15:45:46 +0530

    fixed cross moves of pawn only for occupied target squares

--- src/main/java/org/casestudy/chess/pieces/Pawn.java
+++ src/main/java/org/casestudy/chess/pieces/Pawn.java
@@ -11,6 +11,7 @@ import org.casestudy.chess.core.Square;
 public class Pawn extends Piece {
 
     private MovementDirection direction;
+
     public Pawn(PieceColor pieceColor, MovementDirection movementDirection, Square current) {
         super(PieceType.Pawn, pieceColor, current);
         this.direction = movementDirection;
@@ -19,53 +20,80 @@ public class Pawn extends Piece {
     public boolean canMoveToSquare(Square targetSquare) {
         switch (direction) {
             case Down: {
-                if(hasMoved) {
-                    int[] ymoves = new int[]{1, 1, 1};
-                    int[] xmoves = new int[]{0, -1, 1};
+                if (hasMoved) {
+                    int[] ymoves = new int[]{1};
+                    int[] xmoves = new int[]{0};
                     for (int i = 0; i < xmoves.length; i++) {
                         Square square = new Square(currentPlace.getX() + xmoves[i], currentPlace.getY() + ymoves[i]);
                         if (MoveUtil.isValidSquare(square) && square.equals(targetSquare)) {
                             return true;
                         }
+                    }
+                    if (validateCrossMove(targetSquare, ymoves, xmoves)) {
+                        return true;
                     }
                     return false;
                     // x+1, y, x+1, y-1, x+1, y+1
                 } else {
                     // x+1, y, x+1, y-1, x+1, y+1, x+2, y
-                    int[] ymoves = new int[]{1, 1, 1, 2};
-                    int[] xmoves = new int[]{0, -1, 1,0};
+                    int[] ymoves = new int[]{1, 2};
+                    int[] xmoves = new int[]{0, 0};
                     for (int i = 0; i < xmoves.length; i++) {
                         Square square = new Square(currentPlace.getX() + xmoves[i], currentPlace.getY() + ymoves[i]);
                         if (MoveUtil.isValidSquare(square) && square.equals(targetSquare)) {
                             return true;
                         }
+                    }
+                    if (validateCrossMove(targetSquare, ymoves, xmoves)) {
+                        return true;
                     }
                     return false;
                 }
             }
             case Up: {
-                if(hasMoved) {
+                if (hasMoved) {
                     // x-1, y, x-1, y-1, x-1, y+1
-                    int[] ymoves = new int[]{-1, -1, -1};
-                    int[] xmoves = new int[]{0, -1, 1};
+                    int[] ymoves = new int[]{-1};
+                    int[] xmoves = new int[]{0};
                     for (int i = 0; i < xmoves.length; i++) {
                         Square square = new Square(currentPlace.getX() + xmoves[i], currentPlace.getY() + ymoves[i]);
                         if (MoveUtil.isValidSquare(square) && square.equals(targetSquare)) {
                             return true;
                         }
+                    }
+                    if (validateCrossMove(targetSquare, ymoves, xmoves)) {
+                        return true;
                     }
                     return false;
                 } else {
                     // x-1, y, x+-1, y-1, x-1, y+1, x-2, y
-                    int[] ymoves = new int[]{-1, -1, -1, -2};
-                    int[] xmoves = new int[]{0, -1, 1,0};
+                    int[] ymoves = new int[]{-1, -2};
+                    int[] xmoves = new int[]{0, 0};
                     for (int i = 0; i < xmoves.length; i++) {
                         Square square = new Square(currentPlace.getX() + xmoves[i], currentPlace.getY() + ymoves[i]);
                         if (MoveUtil.isValidSquare(square) && square.equals(targetSquare)) {
                             return true;
                         }
                     }
+                    if (validateCrossMove(targetSquare, ymoves, xmoves)) {
+                        return true;
+                    }
                     return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean validateCrossMove(Square targetSquare, int[] ymoves, int[] xmoves) {
+        int[] crossMoveY = new int[]{1, 1};
+        int[] crossMoveX = new int[]{-1, 1};
+        if (targetSquare.getOccupiedPiece() != null) {
+            // check cross move
+            for (int i = 0; i < xmoves.length; i++) {
+                Square square = new Square(currentPlace.getX() + xmoves[i], currentPlace.getY() + ymoves[i]);
+                if (MoveUtil.isValidSquare(square) && square.equals(targetSquare)) {
+                    return true;
                 }
             }
         }
